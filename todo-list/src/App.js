98323fb03980026dbaaef9c42be33a1419ba8bc3
Chebel405 <anne.chebel@gmail.com>
2023-07-19 11:45:58 +0200

    Scss correct

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,5 +1,29 @@
+import React from "react";
+import { useState } from "react";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+
 function App() {
-  return <h1>Hello world</h1>;
+  const [todoList, setTodoList] = useState([]);  
+
+  function addTodo(content) {
+    const todo = {
+      id: crypto.randomUUID(),
+      content,
+      done: false,
+      edit: false
+    }
+    setTodoList([...todoList, todo ])
+  }
+  return (
+    <div className="d-flex justify-content-center align-items-center p-20">
+      <div className="card container p-20">
+        <h1 className="mb-20">Todo List</h1>
+        <AddTodo addTodo={ addTodo }/>
+        <TodoList />
+      </div>
+    </div>
+  );
   
 }
 
--- todo-list/src/components/AddTodo.js
+++ todo-list/src/components/AddTodo.js
@@ -1,4 +1,11 @@
-function AddTodo() {
-    return <h2>Add Todo</h2>;
+import { useState } from "react";
+
+function AddTodo({ addTodo}) {
+    return (
+        <div className="d-flex flex-row justify-content-center align-items-center mb-20">
+            <input type="text" placeholder="Ajouter une todo" className="mr-15flex-fill"/>
+            <button className="btn btn-primary">Ajouter</button>
+        </div>
+    );
 }
 export default AddTodo;
